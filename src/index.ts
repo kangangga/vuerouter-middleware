7ecee4bf51d920002fa1b11063998f8c15395b0c
kangangga <kangangga@users.noreply.github.com>
2021-11-08 22:00:31 +0000

    deploy: 36f9af80c579424577fb1574e5e15e8009c05e17

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @category core
- * @since 1.0.1
- */
-export function vuerouterMiddleware(): boolean {
-  return true
-}
--- test/unit/test-vuerouter-middleware.ts
+++
@@ -1,7 +0,0 @@
-import test from 'ava'
-
-import { vuerouterMiddleware } from '../../src/index'
-
-test.skip('TODO: unit-test vuerouter-middleware', (t) => {
-    t.fail()
-})
