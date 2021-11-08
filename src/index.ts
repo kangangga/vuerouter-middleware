e9a6f9d41ba26c47c942a70482fe821f941cafea
Angga Saputra <masangga.com@gmail.com>
2021-11-09 03:49:18 +0700

    up

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,3 @@
-#!/usr/bin/env node
-
-/**
- * vuerouter-middleware
- * vue router
- */
-
-/**
- * TODO: document
- */
 export function vuerouterMiddleware() {
-    // TODO: implement
+  // TODO: implement
 }
-
-// Local Variables:
-// mode: typescript
-// End:
